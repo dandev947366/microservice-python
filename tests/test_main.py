99b2af515f720a605806a5b1410ea622e6820929
Dan Le <dan.dev947366@gmail.com>
2024-12-13 00:04:51 +0200

    add test

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+import os, sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
+from main import app
+client = TestClient(app)
+
+def test_get():
+    expected = "Dan"
+    res = client.get(f"/v1/resource/resource/{expected}")
+    assert expected in res.json()
+
